5306489f694f844e5b032eea52b5db2987eb16a2
Laura Sinclair <l.sinclair@tuta.io>
2024-06-14 15:23:02 +0200

    Send API key with requests (instead of doing it from the frontend)

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const User = require('../models/User.model.js');
 const mongoose = require('mongoose');
 
-router.get('/', (req, res) => {
-	const apiKey = process.env.NYTIMES_API_TOKEN;
-	if (!apiKey) {
-		throw new Error('API key not found');
-	}
+const apiKey = process.env.NYTIMES_API_TOKEN;
+if (!apiKey) {
+	throw new Error('API key not found');
+}
 
+router.get('/', (req, res) => {
 	const page = parseInt(req.query.page) || 0;
 
 	fetch(
-		`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=nature&fq='forests AND wildlife AND climate'&api-key=${process.env.NYTIMES_API_TOKEN}&page=${page}`
+		`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=nature&fq='forests AND wildlife AND climate'&api-key=${apiKey}&page=${page}`
 	)
 		.then((resp) => {
 			return resp.json();
@@ -22,6 +22,31 @@ router.get('/', (req, res) => {
 			const articles = data.response.docs;
 			const totalArticles = data.response.meta.hits;
 			res.status(200).json({ articles, totalArticles });
+		})
+		.catch((error) => {
+			console.log(error.message);
+			res.status(500).send(error.message);
+		});
+});
+
+router.get('/article=:articleSlug', (req, res) => {
+	const { articleSlug } = req.params;
+	const articleId = req.headers['article-id'];
+	// console.log('Article ID:', articleId);
+
+	fetch(
+		`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${articleId}&api-key=${apiKey}`
+	)
+		.then((resp) => {
+			return resp.json();
+		})
+		.then((data) => {
+			console.log(data);
+			if (!data || data.response)
+				throw new Error('Problem fetching article from NY Times API');
+
+			const article = data.response.docs;
+			res.status(200).json({ article });
 		})
 		.catch((error) => {
 			console.log(error.message);
